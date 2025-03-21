aa60985bf8d66514e13627694f375524ddeaa15c
Anna Kodryan <ankodryan@yandex.ru>
2025-03-21 22:28:53 +0700

    init project

---
+++ src/main/java/ru/hits/kt1/Kt1Application.java
@@ -0,0 +1,13 @@
+package ru.hits.kt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kt1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hits/kt1/Kt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hits.kt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
